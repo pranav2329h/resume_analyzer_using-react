b2bdeb5c02e5f469dc134d9185c060654724eefd
Pranav Hydrabade <pranav2329h@gmail.com>
2025-09-14 14:17:49 +0530

    some changes with errors

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,17 +1,15 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import {Link} from "react-router";
 
 const Navbar = () => {
-  return (
-    <nav className="navbar">
-      <Link to="/">
-        <p className="text-2xl  font-bold text-gradient">RESUMER</p>
-      </Link>
-      <Link to="/upload" className="primary-button w-fit">
-        upload Resume
-      </Link>
-    </nav>
-  );
-};
-
-export default Navbar;
+    return (
+        <nav className="navbar">
+            <Link to="/">
+                <p className="text-2xl font-bold text-gradient">RESUMIND</p>
+            </Link>
+            <Link to="/upload" className="primary-button w-fit">
+                Upload Resume
+            </Link>
+        </nav>
+    )
+}
+export default Navbar
