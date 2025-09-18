20df70d80076efa56ac48de011b84806983eae7e
Pranav Hydrabade <pranav2329h@gmail.com>
2025-09-18 15:26:18 +0530

    last changes

--- app/root.tsx
+++ app/root.tsx
@@ -40,7 +40,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Meta />
         <Links />
       </head>
-      <body>
+      <body suppressHydrationWarning>
         <script src="https://js.puter.com/v2/"></script>
         {children}
         <ScrollRestoration />
